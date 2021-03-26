d7f2c2dbf09e018388dbfc991ffd4e337d85dfba
Xiaofeng Wang <henrywangxf@me.com>
2021-03-26 09:56:19 +0800

    More update for parallel and cki sending

--- cki-send.py
+++ cki-send.py
@@ -3,10 +3,14 @@
 import argparse
 import json
 import stomp
+import pathlib
 
 
 def main(args):
-    hosts = [(args.broker, args.port)]
+    hosts = [
+        ("messaging-devops-broker01.web.prod.ext.phx2.redhat.com", 61612),
+        ("messaging-devops-broker02.web.prod.ext.phx2.redhat.com", 61612)
+    ]
     conn = stomp.StompConnection12(host_and_ports=hosts,
                                    use_ssl=True,
                                    ssl_key_file=args.ssl_key_file,
@@ -18,19 +22,38 @@ def main(args):
         "summarized_result": "PASS",
         "team_email": "3rd-qe-list@redhat.com",
         "team_name": "Virt-QE-S1",
-        "results": [
-            {
-                "test_name": "LTP_Lite",
-                "test_description": "Cloud platform - {}".format(args.cloud),
-                "test_arch": args.arch,
-                "test_result": args.result,
-                "test_log_url": [
-                    args.log_url
-                ],
-                "test_waived": "True"
-            }
-        ]
+        "results": []
     }
+
+    template = {
+        "test_name": "",
+        "test_description": "Cloud platform - {}".format(args.cloud),
+        "test_arch": "",
+        "test_result": "",
+        "test_log_url": [
+            f"{args.build_url}display/redirect"
+        ],
+        "test_waived": "True"
+    }
+
+    running_path = pathlib.Path.cwd()
+    print(running_path)
+
+    files = running_path.glob('*.result')
+
+    instances = [pathlib.PurePath(x).name.split(".")[:-1] for x in files]
+    print(instances)
+
+    for (x, y) in instances:
+        filename = f"{x}.{y}.result"
+        result = pathlib.Path(running_path / filename ).read_text().strip()
+        new_temp = template.copy()
+        new_temp["test_name"] = f"{args.cloud}-{x}"
+        new_temp["test_description"] = f"Kernel test result on instance {x} of {args.cloud}"
+        new_temp["test_arch"] = y
+        new_temp["test_result"] = result
+        msg["results"].append(new_temp)
+
     msg_json = json.dumps(msg)
     print(msg_json)
     conn.send("/topic/VirtualTopic.eng.cki.results",
@@ -41,14 +64,10 @@ def main(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Send result to VirtualTopic.eng.cki.results')
-    parser.add_argument("--broker", type=str, required=True, help="ActiveMQ Broker hostname")
-    parser.add_argument("--port", type=int, required=True, help="Stomp port")
     parser.add_argument("--ssl_cert_file", type=str, required=True, help="Certification file")
     parser.add_argument("--ssl_key_file", type=str, required=True, help="Private key file")
     parser.add_argument("--pipeline_id", type=int, required=True, help="CKI pipeline ID")
-    parser.add_argument("--arch", type=str, required=True, help="Test arch")
-    parser.add_argument("--log_url", type=str, required=True, help="Test log URL")
-    parser.add_argument("--result", type=str, required=True, help="Test result")
+    parser.add_argument("--build_url", type=str, required=True, help="Jenkins BUILD URL")
     parser.add_argument("--cloud", type=str, required=True, help="Cloud platform")
     args = parser.parse_args()
 
